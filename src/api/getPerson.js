06bc15eaf44b6b6e83994e3c3a53852700001ee7
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-11-09 11:25:27 -0800

    add async

--- src/api/getPerson.js
+++ src/api/getPerson.js
@@ -1,19 +1,27 @@
 import axios from 'axios'
 
-const getPerson = () => {
-    //The promise way
-    return axios
-        .get("https://randomuser.me/api/")
-        .then ((res)=> {
-            return res.data.results[0];
-        });
+const getPerson = async () => {
+    //The promise way - return the axios call
+    // return axios
+    
+        // .get("https://randomuser.me/api/")
+        // //  return the data without complications
+        // .then ((res)=> {
+        //     return res.data.results[0];
+        // });
+
+        
         // .then ((res)=> {
         //     return res.results;
         // })
         // .then ((res)=> {
         //     console.log('res: ', res);
         // });
+
+    // The async way
+    const res = await axios.get("https://randomuser.me/api/");
+    return res.data.results[0]; 
     
-  }
+  } 
 
   export default getPerson;
